d330e8b35a573ae2edc427cd509b4db8603859fa
Andras Nemeth <guni82hu@gmail.com>
2016-04-03 21:24:39 +0200

    inline reply

--- app/src/main/java/com/example/andrasnemeth/nnotification/MainActivity.java
+++ app/src/main/java/com/example/andrasnemeth/nnotification/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.andrasnemeth.nnotification;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity  extends AppCompatActivity {
+
+    // Key for the string that's delivered in the action's intent
+    private static final String KEY_TEXT_REPLY = "key_text_reply";
 
     public static final int mId = 666;
     private Bitmap bitmap;
@@ -22,6 +29,15 @@ public class MainActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cannond);
+        handleInlineReply(getIntent());
+    }
+
+    private void handleInlineReply(Intent intent) {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            CharSequence message = "You've just sent the following message: " + remoteInput.getCharSequence(KEY_TEXT_REPLY);
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onPlainButtonClick(View view) {
@@ -38,6 +54,22 @@ public class MainActivity  extends AppCompatActivity {
 
     public void onBigTextStyleButtonClick(View view) {
         showNotification(createNotificationBuilder().setStyle(createBigTextStyle()));
+    }
+
+    public void onInlineReplyButtonClick(View view) {
+
+        RemoteInput remoteInput = new RemoteInput.Builder(KEY_TEXT_REPLY)
+                .setLabel("Reply")
+                .build();
+
+        // Create the reply action and add the remote input
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(R.drawable.ic_launcher,
+                        "Reply inline", getPendingIntent())
+                        .addRemoteInput(remoteInput)
+                        .build();
+
+        showNotification(createNotificationBuilder().addAction(action));
     }
 
     private NotificationCompat.Style createBigTextStyle() {
@@ -72,8 +104,6 @@ public class MainActivity  extends AppCompatActivity {
 
     @NonNull
     private NotificationCompat.Builder createNotificationBuilder() {
-        Intent resultIntent = new Intent(this, MainActivity.class);
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT );
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
@@ -90,10 +120,16 @@ public class MainActivity  extends AppCompatActivity {
                 //.setProgress(0, 0, true)
                 ;
         //alternative configuration way
+        PendingIntent resultPendingIntent = getPendingIntent();
         mBuilder.setContentIntent(resultPendingIntent);
-        mBuilder.addAction(R.drawable.ic_launcher, "First", resultPendingIntent);
-        mBuilder.addAction(R.drawable.ic_launcher, "Second", resultPendingIntent);
+        mBuilder.addAction(R.drawable.ic_launcher, "First action", resultPendingIntent);
+        mBuilder.addAction(R.drawable.ic_launcher, "Second action", resultPendingIntent);
         return mBuilder;
+    }
+
+    private PendingIntent getPendingIntent() {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        return PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT );
     }
 
     private void showNotification(NotificationCompat.Builder mBuilder) {
