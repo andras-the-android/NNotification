39ffb63e90a81655611aa1ba3041c2b092a5b116
Andras Nemeth <guni82hu@gmail.com>
2016-04-05 21:55:15 +0200

    quick settings

---
+++ app/src/main/java/com/example/andrasnemeth/nnotification/quicksettings/NNotificationTileService.java
@@ -0,0 +1,39 @@
+package com.example.andrasnemeth.nnotification.quicksettings;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.widget.Toast;
+
+import com.example.andrasnemeth.nnotification.R;
+
+/**
+ * Created by guni8 on 2016. 04. 05..
+ */
+public class NNotificationTileService extends TileService {
+
+    @Override
+    public int onTileAdded() {
+        return super.onTileAdded();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        Tile tile = getQsTile();
+        if (tile.getState() == Tile.STATE_ACTIVE) {
+            tile.setState(Tile.STATE_INACTIVE);
+            tile.setLabel(getString(R.string.notification_off));
+            Toast.makeText(NNotificationTileService.this, "Tile turned off", Toast.LENGTH_SHORT).show();
+        } else {
+            tile.setState(Tile.STATE_ACTIVE);
+            tile.setLabel(getString(R.string.notification_on));
+            Toast.makeText(NNotificationTileService.this, "Tile turned on", Toast.LENGTH_SHORT).show();
+        }
+        tile.updateTile();
+    }
+}
