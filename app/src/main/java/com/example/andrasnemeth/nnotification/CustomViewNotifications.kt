725d5902fc2e88a761c080d192106793a3483232
Andras Nemeth <Andras.Nemeth4@logmein.com>
2020-01-30 15:21:45 +0100

    update custom notification

--- app/src/main/java/com/example/andrasnemeth/nnotification/CustomViewNotifications.kt
+++ app/src/main/java/com/example/andrasnemeth/nnotification/CustomViewNotifications.kt
@@ -1,5 +1,6 @@
 package com.example.andrasnemeth.nnotification
 
+import android.os.Handler
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 
@@ -7,19 +8,37 @@ import androidx.core.app.NotificationCompat
  * Created by guni8 on 2016. 04. 04..
  */
 class CustomViewNotifications(private val notificationHelper: BaseNotificationHelper) {
+
+    private var remoteViewsBig: RemoteViews? = null
+    private var counter = 0
+    private var builder: NotificationCompat.Builder? = null
+
     fun show() {
         val remoteViews = RemoteViews(notificationHelper.context.packageName, R.layout.notification_custom)
         remoteViews.setTextViewText(R.id.content_title, "This is the title of the custom view notification")
-        val remoteViewsBig = RemoteViews(notificationHelper.context.packageName, R.layout.notification_big_custom)
-        remoteViewsBig.setTextViewText(R.id.content_title, "This is the title of the big custom view notification")
-        remoteViewsBig.setTextViewText(R.id.content_text, "This is the text of the big custom view notification")
-        val builder = notificationHelper.createNotificationBuilder()
+
+        remoteViewsBig = RemoteViews(notificationHelper.context.packageName, R.layout.notification_big_custom)
+        remoteViewsBig!!.setTextViewText(R.id.content_title, "This is the title of the big custom view notification")
+        remoteViewsBig!!.setTextViewText(R.id.content_text, "#${counter++} This is the text of the big custom view notification")
+        builder = notificationHelper.createNotificationBuilder()
                 .setSmallIcon(R.drawable.ic_launcher)
                 .setAutoCancel(true)
                 .setCustomContentView(remoteViews)
                 .setCustomBigContentView(remoteViewsBig)
                 .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                .setOnlyAlertOnce(true)
         notificationHelper.showNotification(builder, NOTIFICATION_ID_CUSTOM)
+
+        counter = 0
+        update()
+    }
+
+    private fun update() {
+        Handler().postDelayed({
+            remoteViewsBig!!.setTextViewText(R.id.content_text, "#${counter++} This is the text of the big custom view notification")
+            notificationHelper.showNotification(builder, NOTIFICATION_ID_CUSTOM)
+            if (counter <= 30) update()
+        }, 1000)
     }
 
     companion object {
